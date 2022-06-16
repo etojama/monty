70a9b4c94c5ec701212c25d23af5669c2f8d79f1
etojama <torohkiss@gmail.com>
2022-06-16 15:11:45 -0700

    updated monty

--- error_handl.c
+++ error_handl.c
@@ -2,6 +2,13 @@
 
 void h_error(char *message, char *variable)
 {
-	dprintf(STDERR_FILENO, message, variable);
+	if (!var_1)
+	{
+		dprintf(STDERR_FILENO, message, var_2);
+	}
+	else
+	{
+		dprintf(STDERR_FILENO, message, var_1, var_2);
+	}
 	exit(EXIT_FAILURE);
 }
